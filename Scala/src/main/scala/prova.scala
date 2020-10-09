e308e92568fbb2c75a6a2311bc53ed25698538fd
GiovanniFausto <giovanni.f.95@hotmail.it>
2020-10-09 09:13:00 +0200

    add categoria dom da inviare

--- Scala/src/main/scala/prova.scala
+++ Scala/src/main/scala/prova.scala
@@ -7,36 +7,38 @@ import scala.collection.mutable._
 import scala.io.BufferedSource
 import scala.util.Random._
 import scala.io.StdIn._
-/*TODO da mandare nome cognome IdDomande PunteggiDomande RIposteSelezionate DomandeUscite*/
+
 
 
 object Hello extends App {
   var path="src/main/scala/questionario.csv"
-  var obj= new lettoreDomande(path=path,1)// il numero è per devidere quante domande per ogni categoria
+  var obj= new lettoreDomande(path=path,2)// il numero è per devidere quante domande per ogni categoria
   var lista=obj.listaDom
   //println(lista)
   var IdDomande=List[Int]()//metto gli id delle domande
   var PunteggioDomande=List[Double]() //metto le risposte
   var RisposteSelezionate=List[String]()//metto le risposte scelte
   var DomandeUscite=List[String]()//metto le domande
+  var CategorieDomande=List[String]()
   /*println(lista)
   println(lista.length)*/
   val nome=readLine("inserisci nome: ")
   val cognome=readLine("inserisci cognome: ")
   println(nome,cognome)
 
-  for(i<-lista.indices){
-    var risp = LinkedHashMap[String,String]()
-    var risposta = -1
-    val h=lista(i).toList.head._2.toInt
+  for(i<-lista.indices){//serve per scorrere tra le domande
+    var risp = LinkedHashMap[String,String]() //metto le risposte quando poi faccio lo shuffle, ci saranno 4 risposte
+    var risposta = -1 //metto il numero della risposta
+    val h=lista(i).toList.head._2.toInt //serve pre prendere id della domanda
     IdDomande=h::IdDomande//tutto sto casino per prendere id che per qualche assurdo motivo non funziona con la mappa
     //prendo solo le risposte e le mischio perchè se no la prima è sempre quella giusta
-    var keys= lista(i).keys.drop(3)// mi servono solo le risposte
-    keys=shuffle(keys)
+    var keys= lista(i).keys.drop(3)// mi servono solo le risposte quindi elimino i primi 3 che sono id,cat,dom
+    keys=shuffle(keys) //faccio uno shuffle per mischiare le risposte altrimenti la prima è sempre quella giusta
     for(k <- keys){
       risp+=k->lista(i)(k)//numeroRisposta->risposta
     }
-    DomandeUscite=lista(i)("Domanda")::DomandeUscite
+    DomandeUscite=lista(i)("Domanda")::DomandeUscite //metto le domande che escono
+    CategorieDomande=lista(i)("Categoria")::CategorieDomande
     println("-"*150)
     println("CATEGORIA:\t"+lista(i)("Categoria"))
     println("DOMANDA:\t"+lista(i)("Domanda"))
@@ -73,16 +75,20 @@ object Hello extends App {
   RisposteSelezionate=RisposteSelezionate.reverse
   IdDomande=IdDomande.reverse
   PunteggioDomande=PunteggioDomande.reverse
+  CategorieDomande=CategorieDomande.reverse.distinct //serve per rimuovere i doppioni
   //print(PunteggioDomande)
   println("ID DOMANDE USCITE:\t"+IdDomande)
   println("DOMANDE USCITE:\t"+DomandeUscite)
   println("RISPOSTE SELEZIONATE:\t"+RisposteSelezionate)
   println("PUNTEGGIO RISPOSTE:\t"+PunteggioDomande)
+  println("PUNTEGGIO TOTALE:\t"+PunteggioDomande.sum)
+  println("CATEGORIE DOMANDE:\t"+CategorieDomande)
 
   val msg=  "{"+
     "\"Nome\": \""+ nome + "\", "+
     "\"Cognome\": \""+ cognome + "\", "+
     "\"IdDomande\": " + IdDomande.toArray.mkString("[", ", ", "]") + ", "+
+    "\"CategorieDomande\": "+ CategorieDomande.toArray.mkString("[\"", "\", \"", "\"]") + ", "+
     "\"DomandeUscite\": "+ DomandeUscite.toArray.mkString("[\"", "\", \"", "\"]") + ", "+
     "\"RisposteSelezionate\": "+ RisposteSelezionate.toArray.mkString("[\"", "\", \"", "\"]") + ", "+
     "\"PunteggioDomande\": "+ PunteggioDomande.toArray.mkString("[", ", ", "]") +
