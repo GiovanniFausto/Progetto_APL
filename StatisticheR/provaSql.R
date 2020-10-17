8f5adb615b7756294832737c319dfa40c31576a8
GiovanniFausto <giovanni.f.95@hotmail.it>
2020-10-17 18:23:29 +0200

    add commenti R

--- StatisticheR/provaSql.R
+++ StatisticheR/provaSql.R
@@ -2,26 +2,28 @@
 library(RMySQL)
 library(lubridate)
 
-ora<-Sys.time()
-
+ora<-Sys.time()#prendo il tempo corrente e poi prendo ore e minuti
 ore<-hour(ora)
 minuti<-minute(ora)
-oraCorrente<-paste(ore, minuti,sep=":") 
-target <- "10:00"
+oraCorrente<-paste(ore, minuti,sep=":")
+
+target <- "10:00"#quello da confontare 
 
 if(oraCorrente>target){# controllo se sono oltre un certo orario che significa che ho finito i test e quindi posso fare delle statistiche
   print("E' L'ORA GIUSTA PER FARE LE STATISTICHE")
-  ucscDb <- dbConnect(MySQL(), user="root", password="0000", host="localhost",db="apl")
+  ucscDb <- dbConnect(MySQL(), user="root", password="", host="localhost",db="apl")# serve per la connessione al db
   
-  allTables <- dbListTables(ucscDb)
+  allTables <- dbListTables(ucscDb)# mi ritorna la lista di tutte le tabelle nel db
   
-  dfcandi <- dbReadTable(ucscDb, "dfcandidato")
+  dfcandi <- dbReadTable(ucscDb, "dfcandidato")#prendo le dataframe e tolgo la prima colonna che sono indici della tabella
   dftot <- dbReadTable(ucscDb, "dftot")
   dfcandi<-dfcandi[-c(1)]
   dftot<-dftot[-c(1)]
-  catefgorie<-names(dftot)
+  
+  catefgorie<-names(dftot)# prendo nomi categorie e nomi candidati
   candidati<-dfcandi$candidato
-  #contiene solo i punteggi di ogni categoria per il candidiato 
+  
+  #contiene solo i punteggi di ogni categoria per il candidiato, in praitca rimuovo il nome dei candidati e le domande per categori
   categoriPunteggi<-dfcandi[-c(1:2)]
   #sommo i punteggi delle colonne e che quindi sono i totali per ogni categoria
   sommaColonne<-colSums(dftot)
@@ -34,16 +36,16 @@ if(oraCorrente>target){# controllo se sono oltre un certo orario che significa c
   
   #par(mfrow = c(3,2), mar = c(4, 7, 3, 7))#serve per poter plottare 4 grafici in 2x2 [mar = c(bottom, left, top, right)]
   
-  #------------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   
   # sommo tutti i punteggi di una categoria e li divido per il numero di domande
   #ho qualcosa del tipo 10 punti su 40 domande esempio
-  jpeg("image/plot1.jpg", width = 800, height = 800)
+  jpeg("image/plot1.jpg", width = 800, height = 800)#serve per salvare le immagini
   par(mar=c(5, 13 ,4 ,2))
   barplot(as.matrix(sommaColonne/numerdomande), 
           beside=TRUE,las=2, names.arg =catefgorie, main="Risposte per ogni categoria", xlab="Punteggio", horiz=TRUE) 
   dev.off()
-  #------------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   
   #contiene una dataframe che ha i candidati e la somma dei loro punteggi del test
   output <- data.frame( candidati = dfcandi$candidato ,
@@ -52,24 +54,25 @@ if(oraCorrente>target){# controllo se sono oltre un certo orario che significa c
   output<-output[order(output$punteggitotali),]
   #la inverto cos? i primi sono quelli col punteggio pi? alto 
   output<-output[order(nrow(output):1),] 
-  #output<-head(output,10)
+
   #questo invede ? per plottare i punteggi di ogni candidato, in pratica ? il suo punteggio finale del test
   
   jpeg("image/plot2.jpg", width = 800, height = 800)
-  #output<-head(output,10)
+  #output<-head(output,10) per prendere i primi 10
+  
   #questo invede ? per plottare i punteggi di ogni candidato, in pratica ? il suo punteggio finale del test
   par(mar=c(5, 13 ,4 ,2))
   barplot(output$punteggitotali,
           beside=TRUE,las=2, names.arg = output$candidati, main="Punteggio di ogni candidato", xlab="Punteggio", horiz=TRUE)
   dev.off()
-  #------------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   
   #un grafico semplice a torta simile al ptrimo 
   jpeg("image/plot3.jpg", width = 800, height = 800)
   pie(colSums(dftot)/numerdomande, labels = catefgorie, main="Media punteggi")
   dev.off()
   
-  #------------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   
   #creo una nuova dataframe che ha per indici i nomi dei candidati, mi serve per prendere il massimo e minimo, per colonne le categorie
   df1 <- data.frame(dfcandi[-c(1,2)], row.names = dfcandi$candidato)
@@ -86,7 +89,7 @@ if(oraCorrente>target){# controllo se sono oltre un certo orario che significa c
   barplot(as.matrix(max_min),
           beside=TRUE,las=2, names.arg =names(max_min), main="Punteggio di ogni candidato", xlab="Punteggio", horiz=TRUE)
   dev.off()
-  #------------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   #punteggi ottenuti da tutti i tizi
   totPunteggi <- rowSums(categoriPunteggi)
   #numero partecipanti al test
@@ -104,7 +107,7 @@ if(oraCorrente>target){# controllo se sono oltre un certo orario che significa c
   jpeg("image/plot5.jpg", width = 800, height = 800)
   plot(totPunteggi,y, type="p", lwd= 5, main = "Distribuzione Normale")
   dev.off()
-  #---------------------------------------------------------------------------------------------------------
+  #-------------------------------------------------------------------------------------------------------------------------------------------------------
   
   #salva tutti i plot
   jpeg("image/rdata.jpg")
